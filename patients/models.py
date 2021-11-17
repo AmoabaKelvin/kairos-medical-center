87bca09bb2ab78531bbb24aa0417bcbaf1563f0b
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-17 17:11:28 +0000

    Added a method to get the tests to be performed as a list

--- patients/models.py
+++ patients/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator
+from django.conf import settings
 
+from services.models import LabTests
 
 validate_age = MaxValueValidator(150, "Age cannot be greater than 150")
 
@@ -10,10 +11,11 @@ validate_age = MaxValueValidator(150, "Age cannot be greater than 150")
 class Patient(models.Model):
     patients_name = models.CharField(max_length=200)
     patients_age = models.PositiveIntegerField(validators=[validate_age])
-    patients_contact = models.IntegerField()
+    patients_contact = models.CharField(max_length=10)
+    email_address = models.EmailField(max_length=150)
     tests_to_carry = models.TextField(max_length=500)
     date_added = models.DateField(default=timezone.now)
-    receptionist = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    receptionist = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.patients_name
@@ -28,6 +30,14 @@ class Patient(models.Model):
             .split(",")
         )
         return convert(self.tests_to_carry)
+
+    def get_total_test_cost(self):
+        tests = self.list_of_tests()
+        single_cost = []
+        for test in tests:
+            print(test)
+            single_cost.append(LabTests.objects.get(name__icontains=test.strip()).price)
+        return sum(single_cost)
 
 
 # @receiver(signal=pre_save, sender=Patient)
