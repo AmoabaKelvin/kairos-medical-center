c1a3c6234821a324b168334b28cae6bee00dbb25
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-17 17:19:19 +0000

    Created a LabTests model to handle the addition  of lab tests performed by the laboratory as well as their respective prices.

---
+++ services/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class LabTests(models.Model):
+    """
+    The tests performed at the laboratory and their respective prices
+    """
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    date_added = models.DateTimeField(auto_now_add=timezone.now)
+
+    def __str__(self):
+        return f"{self.name}-{self.price}"
+    
+
+    
