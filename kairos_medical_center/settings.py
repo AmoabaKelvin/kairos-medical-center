c39cf12be93be85499c6faa6a140b9c41b3d6081
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-26 20:08:35 +0000

    Added db conn

--- kairos_medical_center/settings.py
+++ kairos_medical_center/settings.py
@@ -98,6 +98,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
