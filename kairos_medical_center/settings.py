6db9a9327745066a1f242de9ddcc6df8a41a285f
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-26 20:28:36 +0000

    Set debug to True

--- kairos_medical_center/settings.py
+++ kairos_medical_center/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c+6p2)z8d1^b2ef02=tkov*&%x9*s4#jm1quloiu@vyc(i^57)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kairos-medical-center.herokuapp.com', '127.0.0.1']
 
