383232c43c0fcc340ecbba13aeb01de559b421fd
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-29 11:02:23 +0000

    Added django-select2 to the urls

--- kairos_medical_center/urls.py
+++ kairos_medical_center/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("select2/", include("django_select2.urls")),
     path("", include("reception.urls"), name="reception"),
     path('accounts/', include('accounts.urls')),
     path('services/', include('services.urls')),
