e75f2902fce71fe76df40a172c8ab810d45af825
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-11-29 11:07:38 +0000

    changed staticroot folder

--- kairos_medical_center/settings.py
+++ kairos_medical_center/settings.py
@@ -141,10 +141,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
