0431f0c1c4917b806678be73427351495a6c81e3
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-12-01 16:42:42 +0000

    Added url routing to the reception and also the homepage

--- kairos_medical_center/urls.py
+++ kairos_medical_center/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from common.views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
-    path("", include("reception.urls"), name="reception"),
+    path("reception/", include("reception.urls"), name="reception"),
     path('accounts/', include('accounts.urls')),
     path('services/', include('services.urls')),
     path('management/', include('management.urls')),
