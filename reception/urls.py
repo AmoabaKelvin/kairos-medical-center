5e1ad3ab1301ea74f62cfcd5672365c688ba5dbc
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-12-01 16:34:47 +0000

    Removed urls that map to removed views

--- reception/urls.py
+++ reception/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('reception/', views.ReceptionHomeView.as_view(), name='reception_home'),
-    path('createnew/', views.create_receptionist, name='create_receptionist'),
+    path('', views.ReceptionHomeView.as_view(), name='reception_home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('info/<int:pk>', views.PatientDetailAndEditView.as_view(), name='info'),
-    path('search/', views.search_for_patient, name='search_view'),
 ]
