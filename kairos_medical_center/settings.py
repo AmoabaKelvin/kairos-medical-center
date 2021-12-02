16f8bff17c1bc2c8a7c90216ea29f589e36dc88d
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-12-02 08:46:48 +0000

    Added code to configure heroku postgres when debug is False

--- kairos_medical_center/settings.py
+++ kairos_medical_center/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         }
     }
 
-if DEBUG is False:
+if DEBUG == "False":
     import dj_database_url
     prod_db  =  dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(prod_db)
