b4884333c0374ba525ef676a31cbf734ea64689f
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-12-02 12:46:42 +0000

    Changed file path for retriving uploads

--- common/tasks.py
+++ common/tasks.py
@@ -28,7 +28,7 @@ def send_mail(file_name: str, recipient: str, subject: str, message: str):
     if file_name:
         # to_send.attach_file(default_storage.path(file_name))
         from pathlib import Path
-        to_send.attach_file(os.path.join(Path('kairos_medical_center/media'), file_name))
+        to_send.attach_file(os.path.join(Path('app/media'), file_name))
         # to_send.attach_file(os.path.join(settings.MEDIA_ROOT, file_name))
     to_send.send(fail_silently=False)
     # After sending the file as an attachment, delete it from the media folder.
