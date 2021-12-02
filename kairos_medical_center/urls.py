eb8635817fc9c4654f5569752d0d3d3b81fcd48f
Kelvin Amoaba <kelvinamoaba@gmail.com>
2021-12-02 22:20:18 +0000

    Removed media

--- kairos_medical_center/urls.py
+++ kairos_medical_center/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from common.views import homepage
 from django.conf.urls import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', homepage, name='homepage'),
@@ -14,4 +14,4 @@ urlpatterns = [
     path('management/', include('management.urls')),
     path('common/', include('common.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
